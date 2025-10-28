b227d6dc730bfb17db4c4e57dc7e56af592776d9
Abdelrahman Elshouny <elshouny7@gmail.com>
2025-10-29 01:08:05 +0300

    added the design system assets and styles (will integrate later)

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Product } from '../../core/models/product';
+import { Category } from '../../core/models/category';
+import { ProductService } from '../../core/services/product';
+import { CategoryService } from '../../core/services/category';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +11,31 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
+export class Home implements OnInit {
+  private productService = inject(ProductService);
+  private categoryService = inject(CategoryService);
 
+  isLoading = false;
+  errorMessage = '';
+
+  categories$: Observable<Category[]> = of([]);
+  featuredProducts$: Observable<Product[]> = of([]);
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData() {
+    // this.isLoading = true;
+    // this.categories$ = this.categoryService.getAll().pipe(catchError(() => of([])));
+    // this.featuredProducts$ = this.productService.getAll().pipe(
+    //   map((products) => products.slice(0, 8)),
+    //   tap(() => (this.isLoading = false)),
+    //   catchError((err) => {
+    //     this.isLoading = false;
+    //     this.errorMessage = 'Failed to load products.';
+    //     return of([]);
+    //   })
+    // );
+  }
 }

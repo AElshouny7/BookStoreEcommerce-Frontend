2b2ada15b5a69e7c2cced26fb8b71b32577c6c06
Abdelrahman Elshouny <elshouny7@gmail.com>
2025-10-29 01:47:21 +0300

    Refactor app initialization and enhance auth interceptor for improved token handling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,18 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { provideRouter, RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/navbar/navbar';
 import { AuthService } from './core/services/auth';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/interceptors/auth-interceptor';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.scss',
+  standalone: true,
 })
 export class App implements OnInit {
   protected readonly title = signal('Shounzeetos');
@@ -18,3 +23,7 @@ export class App implements OnInit {
     this.auth.initFromStorage();
   }
 }
+
+bootstrapApplication(App, {
+  providers: [provideRouter(routes), provideHttpClient(withInterceptors([authInterceptor]))],
+});

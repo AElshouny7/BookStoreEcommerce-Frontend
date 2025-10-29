87382c07cb1d896e2ed8477470607c073cd894fa
Abdelrahman Elshouny <elshouny7@gmail.com>
2025-10-30 01:25:11 +0300

    refactor: remove home component and update product routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,11 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./features/home/home').then((m) => m.Home),
-  },
-  {
-    path: 'products',
     loadComponent: () =>
       import('./features/products/product-list/product-list').then((m) => m.ProductList),
   },
+  { path: 'products', redirectTo: '', pathMatch: 'full' },
+
   {
     path: 'products/:id',
     loadComponent: () =>
